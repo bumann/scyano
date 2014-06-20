38c8db04ebb791fcd5f79be418b73c13f76f07b3
bumann <bumann@gmx.ch>
2014-06-20 10:14:02 +0200

    description added

--- Scyano/Properties/AssemblyInfo.cs
+++ Scyano/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Resources;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Scyano")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Simple helper that provides message queuing for any class.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Bumann")]
 [assembly: AssemblyProduct("Scyano")]
